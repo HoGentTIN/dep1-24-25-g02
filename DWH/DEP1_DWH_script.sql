cdf376504151b4322f7d5b6c40bcd5daab75e21d
JolanGregoire <jolan.gregoire@student.hogent.be>
2025-03-02 18:04:57 +0100

    script changes

--- DWH/DEP1_DWH_script.sql
+++ DWH/DEP1_DWH_script.sql
@@ -12,7 +12,8 @@ CREATE TABLE [dbo].[DimEnergyContract] (
 );
 
 CREATE TABLE [dbo].[DimWeatherStation] (
-    [WeatherStationID] INT IDENTITY(1,1) PRIMARY KEY,
+    [WeatherStationKey] INT IDENTITY(1,1) PRIMARY KEY,
+    [WeatherStationID] VARCHAR(255),
     [WeatherStationName] VARCHAR(255),
     [Latitude] DECIMAL(9,6),
     [Longitude] DECIMAL(9,6),
@@ -30,57 +31,60 @@ CREATE TABLE [dbo].[DimTime] (
 CREATE TABLE [dbo].[DimDate] (
     [DateKey] INT PRIMARY KEY,
     [FullDate] DATE NOT NULL,
-    [MonthName] VARCHAR(50),
-    [MonthNumber] INT,
     [DayName] VARCHAR(50),
-    [DayNumber] INT,
+    [MonthNameDutch] VARCHAR(50),
+    [MonthNameEN] VARCHAR(50),
+    [DayNameDutch] VARCHAR(50),
+    [DayNameEN] VARCHAR(50),
     [QuarterName] VARCHAR(50),
     [QuarterNumber] INT
 );
 
 CREATE TABLE [dbo].[DimUser] (
     [UserKey] INT IDENTITY(1,1) PRIMARY KEY,
-    [CAN_ID] VARCHAR(255),
+    [EAN_ID] VARCHAR(255),
     [ContractCategory] VARCHAR(255),
-    [DistributionCategory] VARCHAR(255),
-    [RetailProvider] VARCHAR(255),
-    [HasPPAContract] BIT
+    [PVInstallationIndicator] BIT,
+    [ElectricVehicleIndicator] BIT,
+    [HeatPumpIndicator] BIT
 );
 
 -- Fact Tables
 CREATE TABLE [dbo].[FactWeather] (
     [WeatherKey] INT IDENTITY(1,1) PRIMARY KEY,
     [DateKey] INT NOT NULL,
+    [TimeKey] INT NOT NULL,
     [WeatherStationKey] INT NOT NULL,
-    [PrecipProbability] DECIMAL(5,2),
-    [PrecipIntensity] DECIMAL(5,2),
-    [Temperature] DECIMAL(5,2),
-    [TempMin] DECIMAL(5,2),
+    [PrecipQuantity] DECIMAL(5,2),
+    [TempAvg] DECIMAL(5,2),
     [TempMax] DECIMAL(5,2),
-    [TempFeelsLike] DECIMAL(5,2),
-    [TempDewPoint] DECIMAL(5,2),
-    [TempMinDiff8to9h] DECIMAL(5,2),
-    [TempFeelsLikeAvg] DECIMAL(5,2),
-    [TempFeelsLikeMorning] DECIMAL(5,2),
-    [TempFeelsLikeAfternoon] DECIMAL(5,2),
-    [WindSpeed] DECIMAL(5,2),
-    [WindGust] DECIMAL(5,2),
-    [WindSpeedAvg] DECIMAL(5,2),
-    [WindDirection] INT,
-    [ShortWaveRadiationAvg] DECIMAL(5,2),
-    [SunFraction] DECIMAL(5,2)
+    [TempMin] DECIMAL(5,2),
+    [TempGrassPt100Avg] DECIMAL(5,2),
+    [TempSoilAvg] DECIMAL(5,2),
+    [TempSoilAvg5cm] DECIMAL(5,2),
+    [TempSoilAvg10cm] DECIMAL(5,2),
+    [TempSoilAvg20cm] DECIMAL(5,2),
+    [TempSoilAvg50cm] DECIMAL(5,2),
+    [WindSpeed10m] DECIMAL(5,2),
+    [WindSpeedAvg30m] DECIMAL(5,2),
+    [WindGustsSpeed] DECIMAL(5,2),
+    [HumidityRelShelterAvg] DECIMAL(5,2),
+    [Pressure] DECIMAL(7,2),
+    [SunDuration] DECIMAL(5,2),
+    [ShortWaveFromSkyAvg] DECIMAL(5,2),
+    [SunIntAvg] DECIMAL(5,2)
 );
 
-CREATE TABLE [dbo].[FactBifex] (
-    [BifexKey] INT IDENTITY(1,1) PRIMARY KEY,
+CREATE TABLE [dbo].[FactBelpex] (
+    [BelpexKey] INT IDENTITY(1,1) PRIMARY KEY,
     [DateKey] INT NOT NULL,
-    [UserKey] INT NOT NULL,
-    [BifexPrice] DECIMAL(10,4),
-    [Bifex_PLP_Uplift] DECIMAL(10,4)
+    [TimeKey] INT NOT NULL,
+    [BelpexPrice] DECIMAL(10,4),
+    [Belpex_RLP_MPrice] DECIMAL(10,4)
 );
 
 CREATE TABLE [dbo].[FactEnergyUsage] (
-    [EnergyUsageKey] INT IDENTITY(1,1) PRIMARY KEY,
+    [UsageKey] INT IDENTITY(1,1) PRIMARY KEY,
     [DateKey] INT NOT NULL,
     [TimeKey] INT NOT NULL,
     [UserKey] INT NOT NULL,
@@ -92,66 +96,37 @@ CREATE TABLE [dbo].[FactNetworkCosts] (
     [NetworkCostKey] INT IDENTITY(1,1) PRIMARY KEY,
     [DateKey] INT NOT NULL,
     [UserKey] INT NOT NULL,
-    [NetworkSegment] VARCHAR(255),
+    [NetworkOperator] VARCHAR(255),
     [TransmissionCost] DECIMAL(10,2),
-    [CongestionTariff_Daylight] DECIMAL(10,2),
-    [CongestionTariff_Daylight_Normal] DECIMAL(10,2),
-    [CongestionTariff_Daylight_ReducedHeight] DECIMAL(10,2),
-    [MarketCost] DECIMAL(10,2),
-    [CapacityTariff] DECIMAL(10,2),
-    [CapacityTariff_ClassMarket] DECIMAL(10,2),
-    [CapacityTariff_ClassMarket_Normal] DECIMAL(10,2),
-    [ProcurementTariff] DECIMAL(10,2),
-    [DataManagementTariff_Supplier] DECIMAL(10,2),
-    [DataManagementTariff_QueryForOtherMarkets] DECIMAL(10,2)
+    [CapacityTariff_DigitalMeter] DECIMAL(10,2),
+    [ConsumptionTariff_DigitalMeter_Normal] DECIMAL(10,2),
+    [ConsumptionTariff_DigitalMeter_ExclusiveNight] DECIMAL(10,2),
+    [BalancingCost] DECIMAL(10,2),
+    [MeterFactor] DECIMAL(10,2),
+    [CapacityTariff_ClassicMeter] DECIMAL(10,2),
+    [ConsumptionTariff_ClassicMeter_Normal] DECIMAL(10,2),
+    [ConsumptionTariff_ClassicMeter_ExclusiveNight] DECIMAL(10,2),
+    [ProsumerTariff] DECIMAL(10,2),
+    [DataManagementTariff_YearlyMonthlyReadMeters] DECIMAL(10,2),
+    [DataManagementTariff_QuarterlyReadMeters] DECIMAL(10,2)
 );
 
 -- Adding Constraints
-ALTER TABLE [dbo].[FactWeather]  WITH CHECK ADD CONSTRAINT [FK_FactWeather_Date] FOREIGN KEY ([DateKey])
-REFERENCES [dbo].[DimDate]([DateKey]);
-GO
+ALTER TABLE [dbo].[FactWeather] WITH CHECK ADD CONSTRAINT [FK_FactWeather_Date] FOREIGN KEY ([DateKey]) REFERENCES [dbo].[DimDate]([DateKey]);
 ALTER TABLE [dbo].[FactWeather] CHECK CONSTRAINT [FK_FactWeather_Date];
-GO
-ALTER TABLE [dbo].[FactWeather]  WITH CHECK ADD CONSTRAINT [FK_FactWeather_WeatherStation] FOREIGN KEY ([WeatherStationKey]) 
-REFERENCES [dbo].[DimWeatherStation]([WeatherStationID]);
-GO
+ALTER TABLE [dbo].[FactWeather] WITH CHECK ADD CONSTRAINT [FK_FactWeather_Time] FOREIGN KEY ([TimeKey]) REFERENCES [dbo].[DimTime]([TimeKey]);
+ALTER TABLE [dbo].[FactWeather] CHECK CONSTRAINT [FK_FactWeather_Time];
+ALTER TABLE [dbo].[FactWeather] WITH CHECK ADD CONSTRAINT [FK_FactWeather_WeatherStation] FOREIGN KEY ([WeatherStationKey]) REFERENCES [dbo].[DimWeatherStation]([WeatherStationKey]);
 ALTER TABLE [dbo].[FactWeather] CHECK CONSTRAINT [FK_FactWeather_WeatherStation];
-GO
 
-ALTER TABLE [dbo].[FactBifex]  WITH CHECK ADD CONSTRAINT [FK_FactBifex_Date] FOREIGN KEY ([DateKey])
-REFERENCES [dbo].[DimDate]([DateKey]);
-GO
-ALTER TABLE [dbo].[FactBifex] CHECK CONSTRAINT [FK_FactBifex_Date];
-GO
-ALTER TABLE [dbo].[FactBifex]  WITH CHECK ADD CONSTRAINT [FK_FactBifex_User] FOREIGN KEY ([UserKey]) 
-REFERENCES [dbo].[DimUser]([UserKey]);
-GO
-ALTER TABLE [dbo].[FactBifex] CHECK CONSTRAINT [FK_FactBifex_User];
-GO
+ALTER TABLE [dbo].[FactBelpex] WITH CHECK ADD CONSTRAINT [FK_FactBelpex_Date] FOREIGN KEY ([DateKey]) REFERENCES [dbo].[DimDate]([DateKey]);
+ALTER TABLE [dbo].[FactBelpex] CHECK CONSTRAINT [FK_FactBelpex_Date];
+ALTER TABLE [dbo].[FactBelpex] WITH CHECK ADD CONSTRAINT [FK_FactBelpex_Time] FOREIGN KEY ([TimeKey]) REFERENCES [dbo].[DimTime]([TimeKey]);
+ALTER TABLE [dbo].[FactBelpex] CHECK CONSTRAINT [FK_FactBelpex_Time];
 
-ALTER TABLE [dbo].[FactEnergyUsage]  WITH CHECK ADD CONSTRAINT [FK_FactEnergyUsage_Date] FOREIGN KEY ([DateKey]) 
-REFERENCES [dbo].[DimDate]([DateKey]);
-GO
+ALTER TABLE [dbo].[FactEnergyUsage] WITH CHECK ADD CONSTRAINT [FK_FactEnergyUsage_Date] FOREIGN KEY ([DateKey]) REFERENCES [dbo].[DimDate]([DateKey]);
 ALTER TABLE [dbo].[FactEnergyUsage] CHECK CONSTRAINT [FK_FactEnergyUsage_Date];
-GO
-ALTER TABLE [dbo].[FactEnergyUsage]  WITH CHECK ADD CONSTRAINT [FK_FactEnergyUsage_Time] FOREIGN KEY ([TimeKey]) 
-REFERENCES [dbo].[DimTime]([TimeKey]);
-GO
+ALTER TABLE [dbo].[FactEnergyUsage] WITH CHECK ADD CONSTRAINT [FK_FactEnergyUsage_Time] FOREIGN KEY ([TimeKey]) REFERENCES [dbo].[DimTime]([TimeKey]);
 ALTER TABLE [dbo].[FactEnergyUsage] CHECK CONSTRAINT [FK_FactEnergyUsage_Time];
-GO
-ALTER TABLE [dbo].[FactEnergyUsage]  WITH CHECK ADD CONSTRAINT [FK_FactEnergyUsage_User] FOREIGN KEY ([UserKey]) 
-REFERENCES [dbo].[DimUser]([UserKey]);
-GO
+ALTER TABLE [dbo].[FactEnergyUsage] WITH CHECK ADD CONSTRAINT [FK_FactEnergyUsage_User] FOREIGN KEY ([UserKey]) REFERENCES [dbo].[DimUser]([UserKey]);
 ALTER TABLE [dbo].[FactEnergyUsage] CHECK CONSTRAINT [FK_FactEnergyUsage_User];
-GO
-
-ALTER TABLE [dbo].[FactNetworkCosts]  WITH CHECK ADD CONSTRAINT [FK_FactNetworkCosts_Date] FOREIGN KEY ([DateKey]) 
-REFERENCES [dbo].[DimDate]([DateKey]);
-GO
-ALTER TABLE [dbo].[FactNetworkCosts] CHECK CONSTRAINT [FK_FactNetworkCosts_Date];
-GO
-ALTER TABLE [dbo].[FactNetworkCosts]  WITH CHECK ADD CONSTRAINT [FK_FactNetworkCosts_User] FOREIGN KEY ([UserKey]) 
-REFERENCES [dbo].[DimUser]([UserKey]);
-GO
-ALTER TABLE [dbo].[FactNetworkCosts] CHECK CONSTRAINT [FK_FactNetworkCosts_User];
-GO
