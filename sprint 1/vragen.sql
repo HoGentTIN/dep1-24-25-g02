8d2924ddb0aee1f66d72aaa442b6c895c313be2e
Jaak Daemen <jaak.daemen@student.hogent.be>
2025-03-13 02:52:32 +0100

    Script met queries voor vragen toegevoegd

---
+++ sprint 1/vragen.sql
@@ -0,0 +1,123 @@
+SELECT COUNT(*) as 'Aantal' FROM FactEnergyCost JOIN dbo.DimEnergyContract D on FactEnergyCost.ContractKey = D.ContractKey WHERE Provider = 'DATS24' AND ContractName = 'ELEKTRICITEIT'
+
+SELECT DISTINCT SingleMeterVariableMeterFactor FROM FactEnergyCost  JOIN dbo.DimEnergyContract D on FactEnergyCost.ContractKey = D.ContractKey WHERE ContractName = 'ZON WIND FLEX'
+
+SELECT * FROM FactEnergyCost JOIN dbo.DimEnergyContract D on D.ContractKey = FactEnergyCost.ContractKey
+WHERE DateKey LIKE '2024%' AND (AdministrativeCosts = (SELECT MIN(AdministrativeCosts) FROM FactEnergyCost)
+   OR AdministrativeCosts = (SELECT MAX(AdministrativeCosts) FROM FactEnergyCost));
+
+SELECT MIN(SingleMeterFixed) FROM FactEnergyCost
+
+SELECT EnergyCostKey, AdministrativeCosts, DateKey, ContractKey, SingleMeterFixed
+FROM FactEnergyCost
+WHERE SingleMeterFixed = (SELECT MIN(SingleMeterFixed) FROM FactEnergyCost WHERE DateKey / 10000 = 2024)
+   OR SingleMeterFixed = (SELECT MAX(SingleMeterFixed) FROM FactEnergyCost WHERE DateKey / 10000 = 2024);
+
+
+-- Query voor laagste en hoogste AdministrativeCosts met AdministrativeCosts als tweede kolom
+-- Selecteer het contract met de hoogste injectieprijs voor januari 2025
+-- met een Belpex-waarde van 100 euro
+WITH InjectionPrices AS (
+    SELECT
+        f.ContractKey,
+        (100 * ISNULL(f.SingleMeterInjectionMeterFactor, 0) + ISNULL(f.SingleMeterInjectionBalancingCost, 0)) AS SingleMeterInjectionPrice,
+        (100 * ISNULL(f.DualMeterDayInjectionMeterFactor, 0) + ISNULL(f.DualMeterDayInjectionBalancingCost, 0)) AS DualMeterDayInjectionPrice,
+        (100 * ISNULL(f.DualMeterNightInjectionMeterFactor, 0) + ISNULL(f.DualMeterNightInjectionBalancingCost, 0)) AS DualMeterNightInjectionPrice
+    FROM
+        FactEnergyCost f
+    WHERE
+        f.DateKey LIKE '202501%'
+),
+MaxPrices AS (
+    SELECT
+        ContractKey,
+        CASE
+            WHEN SingleMeterInjectionPrice >= DualMeterDayInjectionPrice AND SingleMeterInjectionPrice >= DualMeterNightInjectionPrice THEN SingleMeterInjectionPrice
+            WHEN DualMeterDayInjectionPrice >= SingleMeterInjectionPrice AND DualMeterDayInjectionPrice >= DualMeterNightInjectionPrice THEN DualMeterDayInjectionPrice
+            ELSE DualMeterNightInjectionPrice
+        END AS MaxInjectionPrice,
+        CASE
+            WHEN SingleMeterInjectionPrice >= DualMeterDayInjectionPrice AND SingleMeterInjectionPrice >= DualMeterNightInjectionPrice THEN 'SingleMeter'
+            WHEN DualMeterDayInjectionPrice >= SingleMeterInjectionPrice AND DualMeterDayInjectionPrice >= DualMeterNightInjectionPrice THEN 'DualMeterDay'
+            ELSE 'DualMeterNight'
+        END AS MeterType
+    FROM
+        InjectionPrices
+)
+SELECT TOP 1
+    ContractKey,
+    MaxInjectionPrice AS HoogstePrijsPerKWu,
+    MeterType
+FROM
+    MaxPrices
+ORDER BY
+    MaxInjectionPrice DESC;
+
+
+-- Query om het contract te vinden met de hoogste bijdrage voor groene stroom en WKK samen over 2024
+WITH ContractContributions AS (
+    SELECT
+        ContractKey,
+        SUM(ISNULL(GreenElectricity, 0) + ISNULL(WKK, 0)) AS TotalContribution
+    FROM
+        FactEnergyCost
+    WHERE
+        DateKey >= 20240101 AND DateKey < 20250101
+    GROUP BY
+        ContractKey
+)
+SELECT TOP 1
+    ContractKey,
+    TotalContribution AS HoogsteBijdragePerKWu
+FROM
+    ContractContributions
+ORDER BY
+    TotalContribution DESC;
+
+
+
+-- Laagste meterfactor februari
+WITH MeterfactorData AS (
+    SELECT
+        ContractKey,
+        'SingleMeter' AS MeterType,
+        SingleMeterInjectionMeterFactor AS Meterfactor
+    FROM
+        FactEnergyCost
+    WHERE
+        DateKey LIKE '202502%'
+        AND SingleMeterInjectionMeterFactor IS NOT NULL
+
+    UNION ALL
+
+    SELECT
+        ContractKey,
+        'DualMeterDay' AS MeterType,
+        DualMeterDayInjectionMeterFactor AS Meterfactor
+    FROM
+        FactEnergyCost
+    WHERE
+        DateKey LIKE '202502%'
+        AND DualMeterDayInjectionMeterFactor IS NOT NULL
+
+    UNION ALL
+
+    SELECT
+        ContractKey,
+        'DualMeterNight' AS MeterType,
+        DualMeterNightInjectionMeterFactor AS Meterfactor
+    FROM
+        FactEnergyCost
+    WHERE
+        DateKey LIKE '202502%'
+        AND DualMeterNightInjectionMeterFactor IS NOT NULL
+)
+SELECT TOP 1
+    ContractKey,
+    MeterType,
+    Meterfactor AS LaagsteMeterfactor
+FROM
+    MeterfactorData
+ORDER BY
+    Meterfactor ASC;
+
